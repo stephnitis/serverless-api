abd254a2f0abda2a3f2ff15dc1d2e7fb3e33db1f
Stephanie Hill <stephni.hill@gmail.com>
2022-10-12 16:04:34 -0700

    handleRead

--- handleRead/index.js
+++ handleRead/index.js
@@ -1,11 +1,31 @@
-const dynamoose = require('dynamoose')
+const dynamoose = require('dynamoose');
+
+const peopleSchema = new dynamoose.Schema({
+  id: Number,
+  name: String,
+  breakfast: String,
+});
+
+const peopleModel = dynamoose.model('people', peopleSchema);
 
 exports.handler = async (event) => {
+  console.log('---------->', event.queryStringParameters);
 
-    console.log('---------->', event.queryStringParameters)
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+  let response = {
+    statusCode: null,
+    body: null,
+  };
+
+  try {
+    let peopleRecords = await peopleModel.scan().exec();
+    response.statusCode = 200;
+    response.body = JSON.stringify(peopleRecords);
+  } catch(e){
+    console.log(e);
+    response.statusCode = 500;
+    response.body = JSON.stringify(e.message);
+  }
+
+  return response;
+
 };
